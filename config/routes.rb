c8037ff908d4d213b42c70ec62b933129ff6eb1b
EC2 Default User <ec2-user@ip-172-31-3-163.ec2.internal>
2022-01-10 06:19:05 +0000

    books controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/create'
+  get 'books/show'
+  get 'books/edit'
+  get 'books/update'
+  get 'books/destroy'
   devise_for :users
   root "homes#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
