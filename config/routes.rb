b636da3c6fb636625a77b7303d512dc382b43133
EC2 Default User <ec2-user@ip-172-31-3-163.ec2.internal>
2022-01-11 16:30:16 +0000

    [Finish]ログイン、新規作成、ログアウト時のフラッシュ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   root "homes#top"
   get "homes/about" => "homes#about", as: "homes_about"
   resources :books, except: [:new]
