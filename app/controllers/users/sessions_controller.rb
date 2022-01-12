994a65e69bf3fb61ff502c1a0f47340f98add8b9
EC2 Default User <ec2-user@ip-172-31-3-163.ec2.internal>
2022-01-12 02:56:43 +0000

    [Fix]不要だったのでUsers::SessionsControllerをコメントアウトに戻す

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -9,10 +9,9 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   #POST /resource/sign_in
-  def create
-    super
-    flash[:notice] = "Signed in successfully."
-  end
+  # def create
+  #   super
+  # end
 
   # DELETE /resource/sign_out
   # def destroy
