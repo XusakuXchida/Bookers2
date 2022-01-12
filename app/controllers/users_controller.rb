058d4862cee118bbabf3a8bc85dbbb36ab8587f9
EC2 Default User <ec2-user@ip-172-31-3-163.ec2.internal>
2022-01-12 16:53:07 +0000

    [Finish]users_controllerにrequire_current_user作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_current_user, only: [:edit, :update]
 
   def index
     @users = User.all
@@ -30,5 +31,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  def require_current_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
   end
 end
