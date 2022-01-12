7395d66fdc2dec192beaf3ad12a10bf7698e2bdf
EC2 Default User <ec2-user@ip-172-31-3-163.ec2.internal>
2022-01-12 16:56:33 +0000

    [Finish]books_controllerにrequire_current_user作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
+  before_action :require_current_user, only: [:edit, :update, :destroy]
 
   def index
     @book = Book.new
@@ -47,5 +48,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def require_current_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      redirect_to books_path
+    end
   end
 end
